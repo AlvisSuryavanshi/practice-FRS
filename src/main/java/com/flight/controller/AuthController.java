c442e13154f262e71b871306fc3a962f0b90c419
Mohammed Sufiyan <mohammedskhan92@gmail.com>
2023-09-14 15:23:33 +0530

    signup method added.

---
+++ src/main/java/com/flight/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.flight.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @RequestMapping("/signup")
+    public String signup(){
+        return "Signup page.";
+    }
+}
