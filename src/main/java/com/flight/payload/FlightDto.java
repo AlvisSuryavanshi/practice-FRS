2e2fd30f544b169e11bf4da001710a619c48fd5e
AlvisSuryavanshi <alvis.surya@gmail.com>
2023-09-15 16:18:13 +0530

    adding flightDto class

---
+++ src/main/java/com/flight/payload/FlightDto.java
@@ -0,0 +1,24 @@
+package com.flight.payload;
+
+public class FlightDto {
+
+    private String flightName;
+
+    private long flightNumber;
+
+    public String getFlightName() {
+        return flightName;
+    }
+
+    public void setFlightName(String flightName) {
+        this.flightName = flightName;
+    }
+
+    public long getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(long flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+}
